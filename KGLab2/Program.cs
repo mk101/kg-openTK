1b3d09358510d81a1c7fa6c03a037e4fbe023b01
Maxim Kolesov <pro100maks222@gmail.com>
2022-03-25 16:34:18 +0300

    Add window

---
+++ KGLab2/Program.cs
@@ -0,0 +1,11 @@
+﻿using KGLab2;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+
+var nativeWindowSettings = new NativeWindowSettings() {
+    Size = new Vector2i(774, 774),
+    Title = "Screensaver собачка",
+};
+
+using var screensaver = new Screensaver(GameWindowSettings.Default, nativeWindowSettings);
+screensaver.Run();
---
+++ KGLab2/Screensaver.cs
@@ -0,0 +1,18 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace KGLab2; 
+
+public sealed class Screensaver : GameWindow {
+    public Screensaver(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) 
+        : base(gameWindowSettings, nativeWindowSettings) { }
+
+    protected override void OnUpdateFrame(FrameEventArgs args) {
+        if (KeyboardState.IsKeyDown(Keys.Escape)) {
+            Close();
+        }
+        
+        base.OnUpdateFrame(args);
+    }
+}
